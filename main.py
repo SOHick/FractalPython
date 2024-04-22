f430d8c3ae1b8fb09b6fdcd8ea61decc8ad38379
SOHick <shnikitaprog@mail.ru>
2024-04-22 21:12:49 +0300

    + fromula

--- main.py
+++ main.py
@@ -3,17 +3,17 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 
-pmin, pmax, qmin, qmax = -2.5, 1.5, -2, 2
+pmin, pmax, qmin, qmax = -2, 2, -2, 2
 # пусть c = p + iq и p меняется в диапазоне от pmin до pmax,
 # а q меняется в диапазоне от qmin до qmax
 
 xpoints, ypoints = 200, 200
 # число точек по горизонтали и вертикали
 
-max_iterations = 500
+max_iterations = 100
 # максимальное количество итераций
 
-infinity_border = 10
+infinity_border = 3
 # если ушли на это расстояние, считаем, что ушли на бесконечность
 
 image = np.zeros((xpoints, ypoints))
@@ -22,13 +22,13 @@ image = np.zeros((xpoints, ypoints))
 
 for ip, p in enumerate(np.linspace(pmin, pmax, xpoints)):
     for iq, q in enumerate(np.linspace(qmin, qmax, ypoints)):
-        c = p + 1j * q
+        z = p + 1j * q
         # буквой j обозначается мнимая единица: чтобы Python понимал, что речь
         # идёт о комплексном числе, а не о переменной j, мы пишем 1j
 
-        z = 0
+
         for k in range(max_iterations):
-            z = 1/(cmath.sin(1/(z ** 2 + c)))
+            z = z**2/(z**2 - 1)
             # Самая главная формула
 
             if abs(z) > infinity_border:
